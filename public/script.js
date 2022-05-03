51bf79e89f9142392d740a9c5fd8a894cba3e34c
Not-Juan-D <oliver2000.han@gmail.com>
2022-05-03 21:42:08 +1000

    Referenced Rob's codes from https://replit.com/@robdongas/InteractiveTaskList

---
+++ public/script.js
@@ -0,0 +1,66 @@
+// code referenced from week 6 tutorial content - source: https://replit.com/@robdongas/InteractiveTaskList
+
+const form = document.getElementById("taskform");
+const button = document.querySelector("#taskform > button")
+var taskInput = document.getElementById("taskInput");
+var tasklist = document.getElementById("tasklist");
+
+var dueDateInput = document.getElementById("dueDateInput");
+var completionTimeInput = document.getElementById("completionTimeInput");
+var estimatedTimeInput = document.getElementById("estimatedTimeInput");
+var priorityInput = document.getElementById("priorityInput");
+
+form.addEventListener("submit", function(event){
+  event.preventDefault();
+  let task = taskInput.value;
+  let dueDate = dueDateInput.value;
+  let completionTime = completionTimeInput.value;
+  let estimatedTime = estimatedTimeInput.value;
+  let priorityRating = priorityInput.options[priorityInput.selectedIndex].value;
+  addTask(task, dueDate, estimatedTime, priorityRating, completionTime, false);
+  console.log(taskList);
+})
+
+var taskListArray = [];
+
+function addTask(taskDescription, dueDate, estimatedTime, priorityRating, completionTime, completionStatus) {
+  let d = new Date();
+  let dateCreated = d.getFullYear();
+  let task = {
+    taskDescription,
+    dueDate,
+    dateCreated,
+    estimatedTime,
+    completionTime,
+    priorityRating,
+    estimatedTime,
+    completionStatus
+  };
+  taskListArray.push(task);
+  renderTask(task);
+}
+
+function renderTask(task){
+  // Create HTML elements
+  let item = document.createElement("li");
+  item.innerHTML = "<p>" + task.taskDescription + "</p>";
+
+  tasklist.appendChild(item);
+
+  // Extra Task DOM elements
+  let delButton = document.createElement("button");
+  let delButtonText = document.createTextNode("Delete Task");
+  delButton.appendChild(delButtonText);
+  item.appendChild(delButton);
+
+
+  // Event Listeners for DOM elements
+  delButton.addEventListener("click", function(event){
+    event.preventDefault();
+    item.remove();
+  })
+
+
+  // Clear the input form
+  form.reset();
+}
